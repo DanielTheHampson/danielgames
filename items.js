9c746c32d156f0efbf5be2f0c22ebdb165936110
DanielTheHampson <102192923+DanielTheHampson@users.noreply.github.com>
2022-03-29 17:56:30 -0400

    Add files via upload

--- items.js
+++ items.js
@@ -1,6 +1,3 @@
-m1 = "m1";
-m2 = "m2";
-
 function enlargeImage(x) {
     x.style.height = "22.5vh";
     x.style.width = "37.5vh";
@@ -9,21 +6,4 @@ function enlargeImage(x) {
 function shrinkImage(x) {
     x.style.height = "15vh";
     x.style.width = "25vh";
-}
-
-function itemClick(x) {
-    if (x == 1) {
-        window.location.href = "saul";
-    }
-    if (x == 2) {
-        alert("item 2 clicked");
-    }
-
-    if (x = m1) {
-        window.location.href = "ff.html";
-    }
-
-    if (x = m2) {
-        window.location.href = "gfp.html";
-    }
 }
